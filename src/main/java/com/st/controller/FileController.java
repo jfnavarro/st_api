b08f73603311b53e2d0a5ca7a70de75fd72b5ce0
Henrik Treadup <henrik.treadup@scilifelab.se>
2016-06-30 09:51:13 +0200

    Started working on FileController.

---
+++ src/main/java/com/st/controller/FileController.java
@@ -0,0 +1,56 @@
+package com.st.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created by henriktreadup on 6/29/16.
+ */
+
+@Controller
+@RequestMapping("/rest/file/**")
+public class FileController {
+
+    private static String FILE_REST_PREFIX = "/rest/file/";
+
+    Logger log = Logger.getLogger(FileController.class);
+
+    private String getPath(HttpServletRequest request) {
+        String path = (String) request.getAttribute( HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+        return path.substring(FILE_REST_PREFIX.length());
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<InputStreamResource> get(HttpServletRequest request) {
+        String path = getPath(request);
+
+        String content = "The contents of the file.";
+        byte[] contentBytes = content.getBytes(StandardCharsets.UTF_8);
+        final int contentLengthOfStream = contentBytes.length;
+
+        InputStream inputStream = new ByteArrayInputStream(contentBytes);
+
+        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setContentLength(contentLengthOfStream);
+        return new ResponseEntity(inputStreamResource, responseHeaders, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void post(HttpServletRequest request) {
+        // Write output stream.
+
+    }
+}
